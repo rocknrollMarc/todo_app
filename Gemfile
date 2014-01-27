30f7efc33303e3e0776984ab3328de2c84daa4d8
rocknrollMarc <marc.bluemner@gmail.com>
2014-01-27 10:27:45 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+gem 'jquery-rails'
+gem 'execjs'
+gem 'libv8'
+gem 'therubyracer' #needed for rspec install
+gem 'devise'
+
+group :test, :development do
+  gem 'annotate'
+  gem 'faker'
+end
+
+gem 'rspec-rails'
+gem 'autotest'
+gem 'autotest-rails-pure'
+gem 'factory_girl_rails'
+gem 'guard-rspec'
+gem 'guard-spork'
+gem 'spork'
+
+group :test do
+  gem 'capybara'
+
+gem 'rb-inotify'
+gem 'libnotify'
+gem 'cucumber-rails'
+gem 'database_cleaner'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoApp::Application.config.secret_key_base = 'd413235c9b237563bcb8ff642f27e6fbe9647835942645215b299f6dca2c42c4e70dad8c38c7988c49345ec77ccd4cdd1ea595d79d6676c0c7848835da213a2b'
